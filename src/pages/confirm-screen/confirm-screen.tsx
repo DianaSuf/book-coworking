dd22017cc6234a7cc54951dd8b8a275f30782945
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2025-02-10 22:30:23 +0500

    fix confirm register

--- src/pages/confirm-screen/confirm-screen.tsx
+++ src/pages/confirm-screen/confirm-screen.tsx
@@ -1,67 +1,23 @@
-// import { useAppDispatch } from "../../hooks"
-// import { confirmRegisterAction } from "../../store/api-actions";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import { useEffect, useState } from "react";
-// import { AppRoute } from "../../const";
-
-// export default function ConfirmScreen() {
-//   const dispatch = useAppDispatch();
-//   const location = useLocation();
-//   const navigate = useNavigate();
-
-//   const queryParams = new URLSearchParams(location.search);
-//   const data = queryParams.get("data");
-
-//   // Локальное состояние для предотвращения повторных запросов
-//   const [isRequestSent, setIsRequestSent] = useState(false);
-
-//   useEffect(() => {
-//     if (data && !isRequestSent) {
-//       setIsRequestSent(true); // Устанавливаем флаг до отправки запроса
-
-//       dispatch(confirmRegisterAction({ message: data }))
-//         .unwrap()
-//         .then(() => {
-//           // navigate(AppRoute.Profile);
-//           console.log("Profile");
-//         })
-//         .catch(() => {
-//           // navigate(AppRoute.NotFound);
-//           console.log("NotFound");
-//         });
-//         navigate(AppRoute.Profile);
-//     }
-//   }, [data, dispatch, navigate, isRequestSent]);
-
-//   return null;
-// }
-import { useAppDispatch } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import { confirmRegisterAction } from "../../store/api-actions";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { AppRoute } from "../../const";
+import { AppRoute, AuthorizationStatus } from "../../const";
+import { getAuthorizationStatus } from "../../store/slices/user-slice";
 
 export default function ConfirmScreen() {
   const dispatch = useAppDispatch();
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const location = useLocation();
   const navigate = useNavigate();
 
   const queryParams = new URLSearchParams(location.search);
   const data = queryParams.get("data");
-
-  // Локальное состояние для предотвращения повторных запросов
   const [isRequestSent, setIsRequestSent] = useState(false);
 
   useEffect(() => {
-    if (data && !isRequestSent) {
-      setIsRequestSent(true); // Устанавливаем флаг, чтобы предотвратить повторный запрос
-
-      // Сохраняем флаг успешной отправки в localStorage, чтобы при перезагрузке компонента не отправлялся новый запрос
-      // if (!sessionStorage.getItem('confirmRegisterSent')) {
-      //   sessionStorage.setItem('confirmRegisterSent', 'true');
-      if (!localStorage.getItem('confirmRegisterSent')) {
-        localStorage.setItem('confirmRegisterSent', 'true');
-
+    if (data && authorizationStatus !== AuthorizationStatus.USER && authorizationStatus !== AuthorizationStatus.ADMIN && !isRequestSent) {
+      setIsRequestSent(true);
         dispatch(confirmRegisterAction({ message: data }))
           .unwrap()
           .then(() => {
@@ -72,11 +28,8 @@ export default function ConfirmScreen() {
             navigate(AppRoute.NotFound);
             console.log("NotFound");
           });
-      } else {
-        console.log("Request already sent");
-      }
     }
-  }, [data, dispatch, navigate, isRequestSent]);
+  }, [authorizationStatus, data, dispatch, isRequestSent, navigate]);
 
   return null;
 }
