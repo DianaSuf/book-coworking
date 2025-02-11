8b4a6932a4c5b1b6cda239720c970f6b38eaf210
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2025-02-11 22:57:28 +0500

    fix confirm

--- src/pages/confirm-screen/confirm-screen.tsx
+++ src/pages/confirm-screen/confirm-screen.tsx
@@ -1,7 +1,42 @@
+// import { useAppDispatch, useAppSelector } from "../../hooks";
+// import { confirmRegisterAction } from "../../store/api-actions";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { AppRoute, AuthorizationStatus } from "../../const";
+// import { getAuthorizationStatus } from "../../store/slices/user-slice";
+
+// export default function ConfirmScreen() {
+//   const dispatch = useAppDispatch();
+//   const authorizationStatus = useAppSelector(getAuthorizationStatus);
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   const queryParams = new URLSearchParams(location.search);
+//   const data = queryParams.get("data");
+//   const [isRequestSent, setIsRequestSent] = useState(false);
+
+//   useEffect(() => {
+//     if (data && authorizationStatus !== AuthorizationStatus.USER && authorizationStatus !== AuthorizationStatus.ADMIN && !isRequestSent) {
+//       setIsRequestSent(true);
+//         dispatch(confirmRegisterAction({ message: data }))
+//           .unwrap()
+//           .then(() => {
+//             navigate(AppRoute.Profile);
+//             console.log("Profile");
+//           })
+//           .catch(() => {
+//             navigate(AppRoute.NotFound);
+//             console.log("NotFound");
+//           });
+//     }
+//   }, [authorizationStatus, data, dispatch, isRequestSent, navigate]);
+
+//   return null;
+// }
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { confirmRegisterAction } from "../../store/api-actions";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { AppRoute, AuthorizationStatus } from "../../const";
 import { getAuthorizationStatus } from "../../store/slices/user-slice";
 
@@ -13,23 +48,27 @@ export default function ConfirmScreen() {
 
   const queryParams = new URLSearchParams(location.search);
   const data = queryParams.get("data");
-  const [isRequestSent, setIsRequestSent] = useState(false);
 
   useEffect(() => {
-    if (data && authorizationStatus !== AuthorizationStatus.USER && authorizationStatus !== AuthorizationStatus.ADMIN && !isRequestSent) {
-      setIsRequestSent(true);
-        dispatch(confirmRegisterAction({ message: data }))
-          .unwrap()
-          .then(() => {
-            navigate(AppRoute.Profile);
-            console.log("Profile");
-          })
-          .catch(() => {
-            navigate(AppRoute.NotFound);
-            console.log("NotFound");
-          });
+    if (
+      data &&
+      authorizationStatus !== AuthorizationStatus.USER &&
+      authorizationStatus !== AuthorizationStatus.ADMIN
+    ) {
+      localStorage.setItem("confirmRegisterStatus", `Запрос отправлен с данными: ${data}`);
+
+      dispatch(confirmRegisterAction({ message: data }))
+        .unwrap()
+        .then(() => {
+          localStorage.setItem("confirmRegisterStatus", "Успешный ответ от сервера, редирект в профиль");
+          navigate(AppRoute.Profile);
+        })
+        .catch(() => {
+          localStorage.setItem("confirmRegisterStatus", "Ошибка при подтверждении регистрации, редирект на NotFound");
+          navigate(AppRoute.NotFound);
+        });
     }
-  }, [authorizationStatus, data, dispatch, isRequestSent, navigate]);
+  }, [data, dispatch, navigate, authorizationStatus]);
 
   return null;
 }
